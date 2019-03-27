949112968536aa32c232587859fbd62606f14802
Luke Abel <luke.abel@customink.com>
2019-03-27 15:26:08 -0400

    creating example_user first last name

--- 03_sample_app/example_user.rb
+++ 03_sample_app/example_user.rb
@@ -1,12 +1,15 @@
 class User
-  attr_accessor :name, :email
+  attr_accessor :first_name,
+    :last_name,
+    :email
 
   def initialize(attributes = {})
-    @name = attributes[:name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @email = attributes[:email]
   end
 
   def formatted_email
-    "#{@name} <#{@email}>"
+    "#{@first_name} #{@last_name} <#{@email}>"
   end
 end
