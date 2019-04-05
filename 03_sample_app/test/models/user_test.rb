9b0e954e710badd82d75d393352030a265847040
Luke Abel <luke.abel@customink.com>
2019-04-05 15:25:56 -0400

    adding user validations and tests

--- 03_sample_app/test/models/user_test.rb
+++ 03_sample_app/test/models/user_test.rb
@@ -18,4 +18,40 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "     "
     assert_not @user.valid?
   end
+
+  test "name should not exceed 50 chars" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not exceed 255 chars" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email should accept valid formats" do
+    valid_addresses = %w[
+      user@example.com
+      USER@foo.com
+      A_US-ER@foo.bar.org
+      first.last@foo.jp
+      alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email should reject invalid formats" do
+    invalid_addresses = %w[
+      user@example,com
+      user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com
+      foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should not be valid"
+    end
+  end
 end
