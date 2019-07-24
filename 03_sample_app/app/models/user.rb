89e23eac40edee93f553ab2955047d160828c073
Luke Abel <luke.abel@customink.com>
2019-07-24 15:33:52 -0400

    relationship model, migration, user has_many

--- 03_sample_app/app/models/user.rb
+++ 03_sample_app/app/models/user.rb
@@ -3,39 +3,42 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
 
   attr_accessor :remember_token, :activation_token, :reset_token
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   before_save :downcase_email
   before_create :create_activation_digest
 
   validates :name,
-    presence: true,
-    length: {maximum: 50}
+            presence: true,
+            length: { maximum: 50 }
 
   validates :email,
-    presence: true,
-    length: {maximum: 255},
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: {case_sensitive: false}
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
   has_secure_password
 
   validates :password,
-    presence: true,
-    length: {minimum: 6},
-    # has_secure_password prevents users from having nil passwords, this just
-    # allows users to edit their info without providing a password
-    allow_nil: true 
+            presence: true,
+            length: { minimum: 6 },
+            # has_secure_password prevents users from having nil passwords, this
+            # allows users to edit their info without providing a password
+            allow_nil: true
 
   # returns the hash digest of the given string
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ?
       BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
