f0145a9a4b4fdcfd2c700cef05f28ac31a02e07b
Luke Abel <luke.abel@customink.com>
2019-08-02 10:02:27 -0400

    follow integration tests

--- 03_sample_app/test/integration/following_test.rb
+++ 03_sample_app/test/integration/following_test.rb
@@ -31,4 +31,26 @@ class FollowingTest < ActionDispatch::IntegrationTest
       post relationships_path, params: { followed_id: @other.id }
     end
   end
+
+  test "should follow a user the ajax way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other.id }, xhr: true
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user the ajax way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
+    end
+  end
 end
